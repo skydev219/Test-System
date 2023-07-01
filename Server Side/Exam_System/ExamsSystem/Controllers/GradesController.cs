8e32347a846338250e53efaf74825170b9c642e0
FatmaGadd <fatma.gaddo@gmail.com>
2023-07-02 00:47:14 +0300

    GradesController

---
+++ Server Side/Exam_System/ExamsSystem/Controllers/GradesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExamsSystem.Models;
+using ExamsSystem.Repository.IEntities;
+using ExamsSystem.DTO;
+using System.Diagnostics;
+
+namespace ExamsSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GradesController : ControllerBase
+    {
+        #region Fields
+        private readonly IGrades _context;
+        #endregion
+
+        #region Constructors
+        public GradesController(IGrades context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Methods
+        #region Get
+
+        // GET: api/Grades
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GradesDTO>>> GetGrades()
+        {
+
+            IEnumerable<Grade> grades = await _context.GetAll();
+            if (grades == null) return NotFound();
+
+            #region Exams DTO init
+            List<GradesDTO> grs = new List<GradesDTO>();
+
+            foreach (var grade in grades)
+            {
+                grs.Add(item: new GradesDTO
+                {
+                    Student = grade.St.Name,
+                    Exam = grade.Exam.Name,
+                    Grade = grade.Grade1
+                });
+            }
+            #endregion
+
+            return Ok(grs);
+        }
+
+        // GET: api/Grades/5
+        [HttpGet("{st_id},{ex_id}")]
+        public async Task<ActionResult<Grade>> GetGrade(int st_id, int ex_id)
+        {
+            Grade? gr = await _context.GetById(st_id,ex_id);
+            if (gr == null) return NotFound();
+            GradesDTO grade = new GradesDTO()
+            {
+                Student = gr.St.Name,
+                Exam = gr.Exam.Name,
+                Grade = gr.Grade1
+            };
+            return Ok(grade);
+        }
+        #endregion
+
+        #region Update
+        // PUT: api/Grades/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        public async Task<IActionResult> PutGrade(Grade grade)
+        {
+            if (grade == null) return NotFound();
+            try
+            {
+                await _context.Update(grade);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (GetGrade(grade.St_ID,grade.Exam_ID) == null)
+                {
+                    return NotFound();
+                }
+            }
+            return NoContent();
+        }
+        #endregion
+
+        #region Add
+        // POST: api/Grades
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Grade>> PostGrade(Grade grade)
+        {
+            if (grade == null) return BadRequest();
+            try
+            {
+                Grade gr = new Grade()
+                {
+                    St_ID = grade.St_ID,
+                    Exam_ID = grade.Exam_ID,
+                    Grade1 = grade.Grade1
+                };
+                await _context.Add(gr);
+                return CreatedAtAction("GetGrade", new {st_id = gr.St_ID,ex_id=gr.Exam_ID}, gr);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        #endregion
+
+        #region Delete
+        // DELETE: api/Grades/5
+        [HttpDelete("{st_id},{ex_id}")]
+        public async Task<IActionResult> DeleteGrade(int st_id,int ex_id)
+        {
+            Grade? gr = await _context.GetById(st_id,ex_id);
+
+            if (gr == null) return NotFound();
+            try
+            {
+                await _context.DeleteById(st_id,ex_id);
+                var response = new
+                {
+                    message = "Deleted Success",
+                    gr
+                };
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ Server Side/Exam_System/ExamsSystem/Repository/Grades/GradesRepository.cs
@@ -0,0 +1,68 @@
+﻿using ExamsSystem.Models;
+using ExamsSystem.Repository.IEntities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ExamsSystem.Repository.Grades
+{
+    public class GradesRepository:IGrades
+    {
+
+        #region Fileds
+        readonly SchoolContext _dbcontext;
+        #endregion
+
+        #region Constructors
+        public GradesRepository(SchoolContext context)
+        {
+            _dbcontext = context;
+        }
+        #endregion
+
+        #region Methods
+
+        #region Get
+        public async Task<List<Grade>> GetAll()
+        {
+            return await _dbcontext.Grades.Include(g => g.St).Include(g => g.Exam).ToListAsync();
+        }
+
+        public async Task<Grade> GetById(int st_id,int ex_id)
+        {
+            return await _dbcontext.Grades.Include(g => g.St).Include(g => g.Exam).FirstOrDefaultAsync(a => a.St_ID == st_id && a.Exam_ID == ex_id);
+        }
+        #endregion
+
+        #region Add
+        public async Task Add(Grade entity)
+        {
+            await _dbcontext.Grades.AddAsync(entity);
+            await _dbcontext.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Update
+        public async Task Update( Grade entity)
+        {
+            _dbcontext.Entry(entity).State = EntityState.Modified;
+            await _dbcontext.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Delete
+        public async Task DeleteById(int st_id, int ex_id)
+        {
+            if (await GetById(st_id,ex_id) != null)
+            {
+                _dbcontext.Grades.Remove(await GetById(st_id, ex_id));
+                await _dbcontext.SaveChangesAsync();
+            }
+        }
+
+        #endregion
+
+
+        #endregion
+    }
+}
