11c63e3ec72c077e4f44383507b5e70c35ace754
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-07-03 03:00:19 +0300

    update

--- Client Side/examination-system/src/app/components/studentlogin/studentlogin.component.ts
+++ Client Side/examination-system/src/app/components/studentlogin/studentlogin.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-studentlogin',
   templateUrl: './studentlogin.component.html',
-  styleUrls: ['./studentlogin.component.css']
+  styleUrls: ['./studentlogin.component.css'],
 })
 export class StudentloginComponent {
-loginForm = new FormGroup({
-  Username : new FormControl('',[Validators.required]),
-  Password : new FormControl('',[Validators.required])
-});
-get GetUserName(){
-  return this.loginForm.controls['Username'];
-}
-get GetPassword(){
-  return this.loginForm.controls['Password'];
-}
-
-
+  loginForm = new FormGroup({
+    Username: new FormControl('', [Validators.required]),
+    Password: new FormControl('', [Validators.required]),
+  });
+  get GetUserName() {
+    return this.loginForm.controls['Username'];
+  }
+  get GetPassword() {
+    return this.loginForm.controls['Password'];
+  }
 }
