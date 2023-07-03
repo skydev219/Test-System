52ca10371630b6f79b31c6f09f269f977ab3216b
skydev219 <skydev.goldday219@gmail.com>
2023-07-03 03:38:04 +0300

    update

--- Client Side/examination-system/src/app/Services/token.service.ts
+++ Client Side/examination-system/src/app/Services/token.service.ts
@@ -1,25 +1,17 @@
 import { Injectable } from '@angular/core';
-// import{CookieService} from'ngx-cookie-service';
 
-// const TokenKey:string = "Token";
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TokenService {
-
-  constructor() { }
-  GetToken(){
-    return localStorage.getItem("Token");
-    // return this.cookieServiece.get("Token"); 
+  constructor() {}
+  GetToken() {
+    return localStorage.getItem('Token');
   }
-  SaveToken(token:string){
-    return localStorage.setItem("Token",token);
-    // this.cookieServiece.set("Token",token);
-    //add isloggedin from login service
+  SaveToken(token: string) {
+    return localStorage.setItem('Token', token);
   }
-  ClearToken(){
-    localStorage.removeItem("Token");
-    // this.cookieServiece.delete("Token");
+  ClearToken() {
+    localStorage.removeItem('Token');
   }
 }
