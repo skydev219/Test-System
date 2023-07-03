0d43d8e4bcf340ca7fa9244f2fe7227f0f1022d8
skydev219 <skydev.goldday219@gmail.com>
2023-07-03 09:39:10 +0300

    exam component

--- Client Side/examination-system/src/app/app-routing.module.ts
+++ Client Side/examination-system/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { StudentLoginComponent } from './components/student-login/student-login.component';
 import { StudentRegisterComponent } from './components/student-register/student-register.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ExamComponent } from './components/exam/exam.component';
 
 const routes: Routes = [
 {path:'',component:HomeComponent},
 {path:'home',component:HomeComponent},
 {path:'student/login',component:StudentLoginComponent},
 {path:'student/register',component:StudentRegisterComponent},
+{path:'exam',component:ExamComponent},
 {path:'**',component:NotfoundComponent}
 ];
 
