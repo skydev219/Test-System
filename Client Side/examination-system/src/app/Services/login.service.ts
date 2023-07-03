f33c3914fa5f88ca7132cca4adb1da063a4c69ba
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-07-03 03:38:04 +0300

    update

--- Client Side/examination-system/src/app/Services/login.service.ts
+++ Client Side/examination-system/src/app/Services/login.service.ts
@@ -6,14 +6,14 @@ import { catchError, throwError } from 'rxjs';
 import { environment } from '../Environment/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-  constructor(private router:Router,private http:HttpClient) { }
-  currentUser:IUser|undefined;
-  logIn(user:string|null,pass:string|null){
-    this.currentUser={userName:user,password:pass};
-    return this.http.get<{token:string}>(environment.contentful.token);
+  constructor(private router: Router, private http: HttpClient) {}
+  currentUser: IUser | undefined;
+  logIn(user: string | null, pass: string | null) {
+    this.currentUser = { userName: user, password: pass };
+    return this.http.get<{ token: string }>(environment.contentful.token);
     // return this.http.post<IUser>( "real login api with diffrentiating between admin and student", this.currentUser)
     // .pipe(
     //   catchError( (err)=>{
@@ -23,11 +23,11 @@ export class LoginService {
     //this.router.navigate(["/products"]);
     // this.TokenService.SaveToken();
   }
-  logOut(){
-    this.currentUser=undefined;
-    this.router.navigate(["/login"]);
+  logOut() {
+    this.currentUser = undefined;
+    this.router.navigate(['/login']);
   }
-  get isLoggedIn():boolean{
+  get isLoggedIn(): boolean {
     if (this.currentUser?.userName && this.currentUser?.password) {
       return true;
     } else {
